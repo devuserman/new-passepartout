4ff94106419761802ccb06d9d4ce8ecbc94aa028
devuserman <yatchina@gmail.com>
2024-04-17 15:57:18 +0200

    Mdf-1

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
-import './Header.css';
+import './Header-min.css';
 import React, { useState } from 'react';
 import { FaBars } from 'react-icons/fa';
 import { useTranslation } from 'react-i18next';
